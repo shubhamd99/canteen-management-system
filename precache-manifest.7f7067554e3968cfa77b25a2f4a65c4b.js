a2d8f8cf98639d64e2e05825b74d9a967d71f61d
Shubham Dhage <shubhamdhage930@gmail.com>
2019-04-17 00:50:08 +0530

    Updates

---
+++ precache-manifest.7f7067554e3968cfa77b25a2f4a65c4b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "39ee7601b0d7a289def5",
+    "url": "/canteen-management-system/static/js/5.39ee7601.chunk.js"
+  },
+  {
+    "revision": "5e12cdf7b01d0fc05e77",
+    "url": "/canteen-management-system/static/css/main.d96361db.chunk.css"
+  },
+  {
+    "revision": "3f9a40b363414c085d19",
+    "url": "/canteen-management-system/static/js/runtime~main.3f9a40b3.js"
+  },
+  {
+    "revision": "4a0d7ee8d28bd4eecaca",
+    "url": "/canteen-management-system/static/js/2.4a0d7ee8.chunk.js"
+  },
+  {
+    "revision": "e19ac53ab721ad6bf5c24aa44aa56200",
+    "url": "/canteen-management-system/static/media/background.e19ac53a.jpg"
+  },
+  {
+    "revision": "2e012a6cb59354b3a4c0",
+    "url": "/canteen-management-system/static/js/3.2e012a6c.chunk.js"
+  },
+  {
+    "revision": "88304f60c2cb59a74bb65366399a6da0",
+    "url": "/canteen-management-system/static/media/pastry.88304f60.png"
+  },
+  {
+    "revision": "76370f782b7fb27cb5b8",
+    "url": "/canteen-management-system/static/js/4.76370f78.chunk.js"
+  },
+  {
+    "revision": "01e1c38313867add148d68f69bd9fcc9",
+    "url": "/canteen-management-system/static/media/dosa.01e1c383.png"
+  },
+  {
+    "revision": "5e12cdf7b01d0fc05e77",
+    "url": "/canteen-management-system/static/js/main.5e12cdf7.chunk.js"
+  },
+  {
+    "revision": "143b9b73655b3908e15b02ca5aec6322",
+    "url": "/canteen-management-system/static/media/burger-logo.143b9b73.jpg"
+  },
+  {
+    "revision": "0040fe3ba203ec0a5ac3f566a472d6b0",
+    "url": "/canteen-management-system/static/media/burger.0040fe3b.png"
+  },
+  {
+    "revision": "ed6f238fdbb5833f504065dcca4eb0ae",
+    "url": "/canteen-management-system/static/media/chineese.ed6f238f.png"
+  },
+  {
+    "revision": "0f1ba2203372b2efa03c3acb3b6c0322",
+    "url": "/canteen-management-system/static/media/samosa.0f1ba220.png"
+  },
+  {
+    "revision": "aba7a897ac8b505ee63775e4ba252174",
+    "url": "/canteen-management-system/static/media/coke.aba7a897.png"
+  },
+  {
+    "revision": "39ee7601b0d7a289def5",
+    "url": "/canteen-management-system/static/css/5.0bdf46ee.chunk.css"
+  },
+  {
+    "revision": "76370f782b7fb27cb5b8",
+    "url": "/canteen-management-system/static/css/4.f73d04a3.chunk.css"
+  },
+  {
+    "revision": "2e012a6cb59354b3a4c0",
+    "url": "/canteen-management-system/static/css/3.1131308e.chunk.css"
+  },
+  {
+    "revision": "a7161249ecdf3ced3b53b542624a4647",
+    "url": "/canteen-management-system/index.html"
+  }
+];
